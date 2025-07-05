8170b7248f6588616393f6599844405eeaf68f65
Ashmn1629 <Mrinal.Anand2021@iem.edu.in>
2025-07-06 00:27:08 +0530

    feat how to properly name variable

---
+++ src/propervariablesname/propervariablename.java
@@ -0,0 +1,13 @@
+package propervariablesname;
+
+public class propervariablename {
+    public static  void  main(String[] args){
+        //case sensitive
+        //can be letters,digit,dollar signs or underscores
+        //Must begin with a letter,dollar sign or underscore
+        //cannot use Java keywords as variable name
+        //camel case
+        String myName="Mrinal";
+        System.out.println(myName);
+    }
+}
