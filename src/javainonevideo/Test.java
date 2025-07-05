a97cc443d22fd62d7ee8cf645e9c6878eb5f7187
Ashmn1629 <Mrinal.Anand2021@iem.edu.in>
2025-07-06 00:06:17 +0530

    feat primitive data types

---
+++ src/javainonevideo/Test.java
@@ -0,0 +1,29 @@
+package javainonevideo;
+
+public class Test {
+    public static void main (String [] args){
+        System.out.println("Hello World");
+        //whole numbers
+            //byte
+            //short
+            //int
+            //long
+
+            byte age1=20;
+            short age2=20;
+            int age=20;
+            long age3=20;
+        //decimal numbers
+            //float
+            //long
+            float mysalary=30.00011f;
+            double yoursalary=40.393939929292;
+
+        //characters
+            //char
+            char myCharacter='m';
+        //booleans
+            //boolean
+            boolean status=false;
+    }
+}
