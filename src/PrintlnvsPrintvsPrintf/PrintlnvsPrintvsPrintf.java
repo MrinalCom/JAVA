999ca4b58a5550a1e2274e6cb6da84a26d3c06b4
Ashmn1629 <Mrinal.Anand2021@iem.edu.in>
2025-07-07 02:22:36 +0530

    feat println vs print vs printf in java

---
+++ src/PrintlnvsPrintvsPrintf/PrintlnvsPrintvsPrintf.java
@@ -0,0 +1,14 @@
+package PrintlnvsPrintvsPrintf;
+
+public class PrintlnvsPrintvsPrintf {
+        public static void main(String [] args){
+            System.out.println("Mrinal");
+            System.out.print("Anand");
+            int a=1;
+            int b=3;
+            String c="ans";
+            System.out.println(c+": "+(a+b)+"\n");
+            System.out.printf("%s of %d and %d is %d",c,a,b,a+b);
+
+        }
+}
