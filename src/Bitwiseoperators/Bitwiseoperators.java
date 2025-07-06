dd1f15f46bb1083fcaf28e4c28f4426df8fc1354
Ashmn1629 <Mrinal.Anand2021@iem.edu.in>
2025-07-07 00:42:51 +0530

    feat bitwise operators in java

---
+++ src/Bitwiseoperators/Bitwiseoperators.java
@@ -0,0 +1,36 @@
+package Bitwiseoperators;
+
+public class Bitwiseoperators {
+    public  static  void  main (String [] args){
+            int a=5;
+            System.out.println(Integer.toBinaryString(a));
+            //operands -->byte short int long
+
+            //and &
+            int c=5&3;
+            System.out.println(c);
+            //or |
+            int b=5|3;
+            System.out.println(b);
+            //xor ^
+            int d=5^3;
+            System.out.println(d);
+            //not ~
+            int e=~5;
+            System.out.println(e);
+            //left shift <<
+            int m=5<<1;
+            System.out.println(m);
+            //right shift >>
+            int n=4>>1;
+            System.out.println(n);
+            //unsigned right shift >>>
+            int j=3>>>1;
+            System.out.println(j);
+
+
+
+
+
+    }
+}
