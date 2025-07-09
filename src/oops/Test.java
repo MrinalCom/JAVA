bde0879a7262f46b4d4dd57c715cbe571b7d6ed0
MrinalCom <Mrinal.Anand2021@iem.edu.in>
2025-07-10 03:04:27 +0530

    fix getters and setters

--- src/oops/Test.java
+++ src/oops/Test.java
@@ -3,11 +3,10 @@ package oops;
 public class Test {
     public  static void main(String [] args){
         Car car=new Car();
-        car.model="i10";
-        car.brand="Honda";
-        car.speed=90;
-        car.color="Red";
+        car.setModel("i10");
+        car.setBrand("Honda");
         car.accelerate(5);
+        System.out.println(car.getModel());
         System.out.println(car.speed);
 
         animal cat=new cat();
