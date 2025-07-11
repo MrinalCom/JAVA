e58fcccdbec09b0a056bdcdf26f9f7dd496c123d
MrinalCom <Mrinal.Anand2021@iem.edu.in>
2025-07-12 01:38:54 +0530

    feat encapsulation in java

---
+++ src/com/mrinal/packages/test/Test.java
@@ -0,0 +1,9 @@
+package com.mrinal.packages.test;
+
+public class Test {
+    public static void main(String[] args){
+        Student s=new Student();
+        s.setAge(10);
+        System.out.println(s.getAge());
+    }
+}
